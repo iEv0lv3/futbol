c95a5dc08d7b084ccffe134efb0389ec870e5fab
Anthony Gomes <code@tonygom.es>
2019-12-19 22:43:40 -0700

    Add basic logic for lowest_scoring_visitor & lowest_scoring_home_team method & winningest_team methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -98,11 +98,69 @@ class StatTracker
     visitor_wins = 0
     total_games = @games_collection.games.length
 
-    @games_collection.games.each do |game|       
+    @games_collection.games.each do |game|
       if game.home_goals.to_i < game.away_goals.to_i
         visitor_wins += 1
       end
     end
     (visitor_wins / total_games.to_f).abs.round(2)
+  end
+
+  def lowest_scoring_visitor
+    team_ids_with_average_scores = {}
+
+    @games_collection.games.each do |game|
+      if team_ids_with_average_scores.has_key?(game.away_team_id)
+        team_ids_with_average_scores[game.away_team_id] << game.away_goals.to_i
+      elsif !team_ids_with_average_scores.has_key?(game.away_team_id)
+        team_ids_with_average_scores[game.away_team_id] = [game.away_goals.to_i]
+      end
+    end
+
+    team_ids_with_average_scores.each do |team|
+    average_away_goals = team[1].sum / team[1].length
+    team_ids_with_average_scores[team[0]] = average_away_goals.to_s
+    end
+
+  end
+
+  def lowest_scoring_home_team
+    team_ids_with_average_scores = {}
+
+    @games_collection.games.each do |game|
+      if team_ids_with_average_scores.has_key?(game.home_team_id)
+        team_ids_with_average_scores[game.home_team_id] << game.home_goals.to_i
+      elsif !team_ids_with_average_scores.has_key?(game.away_team_id)
+        team_ids_with_average_scores[game.home_team_id] = [game.home_goals.to_i]
+      end
+    end
+
+    team_ids_with_average_scores.each do |team|
+    average_away_goals = team[1].sum / team[1].length
+    team_ids_with_average_scores[team[0]] = average_away_goals.to_s
+    end
+
+  end
+
+  def winningest_team
+    team_wins = []
+
+    @games_collection.games.each do |game|
+      if game.home_goals.to_i < game.away_goals.to_i
+        team_wins << game.away_team_id.to_i
+      elsif game.away_goals.to_i < game.home_goals.to_i
+        team_wins << game.home_team_id.to_i
+      end
+    end
+
+    team_wins.max_by {|i| team_wins.count(i)}
+  end
+
+  def best_fans
+    # percentage_home_wins - percentage_visitor_wins.abs
+  end
+
+  def worst_fans
+    # percentage_home_wins - percentage_visitor_wins.abs
   end
 end
