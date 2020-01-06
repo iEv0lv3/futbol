25dbefb5e6a81309cc69009619dec54c46cfa761
Chase.Gaddis <cgaddis36@gmail.com>
2020-01-05 17:23:06 -0700

    building out most_accurate_team methods, pulling joint effort collab on combining games data

--- lib/modules/season_stats.rb
+++ lib/modules/season_stats.rb
@@ -5,33 +5,20 @@ module SeasonStats
   include Calculateable
   include Gatherable
 
-  def biggest_bust(season_id)
-    # in: season_id
-    # out: team_id
-    # team_win_percentage(wins_by_team(@seasons.teams[season_id]))
-    #     team_win_percentage(postseason_games_by_team)
-    #     team_win_percentage(regular_season_games_by_team)
+  # def winningest_coach(season_id)
+  #   hash = 0
+  #   @games.collection.values.map do |game|
+  #     require "pry"; binding.pry
+  #     if game.season == season_id
+  #       hash[game.away_team_id] += game.away_goals.to_i
+  #     end
+  #   end
+  # end
 
-    # @seasons.teams.reduce({}) do |hash, season|
-    #   require 'pry'; binding.pry
-
-    #   season(season_id)
-    # end
-    # get_team_name_by_id(team_id)
-  end
-
-  def winningest_coach(season_id)
-    season_games = @seasons.teams.map do |team|
-      team.last[season_id]
+  def most_accurate_team(season_id)
+    games.collection.values.reduce({}) do |game_id_hash, value|
+      game_id_hash[value.game_id] = value.home_team_id
     end
-
-    season_games = season_games.flatten!
-    require 'pry'; binding.pry
-
-    team_id = team_win_percentage(season_wins_by_team(season_games))
-    team_total_wins = 80
-    team_totaal_games = 120
-
-    get_team_name_by_id(team_id)
   end
+
 end
--- test/season_stats_test.rb
+++ test/season_stats_test.rb
@@ -27,6 +27,10 @@ class SeasonStatsTest < Minitest::Test
     @stat_tracker = Tracker.from_csv(locations)
   end
 
+  def test_most_accurate_team
+    assert_equal 'Montreal Impact', @stat_tracker.most_accurate_team('20122013')
+  end
+
   def test_biggest_bust_method
     skip
     assert_equal 'Montreal Impact', @stat_tracker.biggest_bust('20132014')
@@ -34,6 +38,8 @@ class SeasonStatsTest < Minitest::Test
   end
 
   def test_season_stats_can_get_winningest_coach
+    skip
+    assert_equal 'Claude Julien', @stat_tracker.winningest_coach('20122013')
     assert_equal 'Claude Julien', @stat_tracker.winningest_coach('20132014')
     assert_equal 'Alain Vigneault', @stat_tracker.winningest_coach('20142015')
   end
