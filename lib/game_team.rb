b0e05301a4fc4f91dc06efe9d8da29f2cbce5858
Steve Anderson <steven.a.anderson2012@gmail.com>
2020-01-07 16:10:47 -0700

    Update game_team.rb

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -1,35 +1,17 @@
-class GameTeam
-  attr_reader :game_id,
-              :team_id,
+class GameTeams
+  attr_reader :team_id,
+              :game_id,
               :hoa,
-              :result,
-              :settled_in,
               :head_coach,
-              :goals,
               :shots,
-              :tackles,
-              :pim,
-              :powerPlayOpportunities,
-              :powerPlayGoals,
-              :faceOffWinPercentage,
-              :giveaways,
-              :takeaways
+              :tackles
 
-  def initialize(game_team_data)
-    @game_id = game_team_data[:game_id]
-    @team_id = game_team_data[:team_id]
-    @hoa = game_team_data[:hoa]
-    @result = game_team_data[:result]
-    @settled_in = game_team_data[:settled_in]
-    @head_coach = game_team_data[:head_coach]
-    @goals = game_team_data[:goals]
-    @shots = game_team_data[:shots]
-    @tackles = game_team_data[:tackles]
-    @pim = game_team_data[:pim]
-    @powerPlayOpportunies = game_team_data[:powerPlayOpportunities]
-    @powerPlayGoals = game_team_data[:powerPlayGoals]
-    @faceOffWinPercentage = game_team_data[:faceOffWinPercentage]
-    @giveaways = game_team_data[:giveaways]
-    @takeaways = game_team_data[:takeaways]
+  def initialize(row)
+    @game_id = row[:game_id]
+    @hoa = row[:hoa]
+    @team_id = row[:team_id]
+    @head_coach = row[:head_coach]
+    @shots = row[:shots]
+    @tackles = row[:tackles]
   end
 end
