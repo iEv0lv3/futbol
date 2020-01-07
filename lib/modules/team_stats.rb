bd99f5ca67c7ef5e14f545f6c79e6ddd2d5b5309
Steve Anderson <steven.a.anderson2012@gmail.com>
2020-01-07 16:22:03 -0700

    Update team_stats.rb

--- lib/modules/team_stats.rb
+++ lib/modules/team_stats.rb
@@ -7,39 +7,6 @@ module TeamStats
   include Calculateable
   include SeasonStats
 
-  def best_season(team_id)
-    final_percentage = team_season_wins_hash(team_id)
-    answer = final_percentage.max_by{|season,win_percentage| win_percentage}
-    answer[0]
-  end
-
-  def worst_season(team_id)
-    final_percentage = team_season_wins_hash(team_id)
-    answer = final_percentage.min_by{|season,win_percentage| win_percentage}
-    answer[0]
-  end
-
-  def team_season_wins_hash(team_id)
-    percentage_seasons = Hash.new(0)
-    @seasons.teams[team_id].each do |season|
-      season_key = season[0]
-      team_season = season[1].flatten!
-      stats = win_lose_draw_regular_season(team_id, team_season)
-      final_percentage = team_season_win_percentage(stats, team_id)
-      percentage_seasons[season_key] = final_percentage[:win_percentage]
-    end
-
-    percentage_seasons
-  end
-
-  def team_season_win_percentage(hash, team_id)
-    total_games = (hash[:win] + hash[:loss] + hash[:draw])
-    wins = hash[:win]
-    percentage = ((wins.to_f / total_games) * 100).round(2)
-    hash[:win_percentage] = percentage
-    hash
-  end
-
   def favorite_opponent(team_id)
     team_wins = get_wins_by_opponent(team_id)
     team_games = get_total_games_by_opponent(team_id)
