4dd266c5a44896396810e8a9bb7fac32f7bad5b7
Jordan <jordan.wallace.williams@gmail.com>
2020-01-08 15:21:58 -0700

    Update worst season method to pass rspec

--- lib/modules/team_stats.rb
+++ lib/modules/team_stats.rb
@@ -281,33 +281,34 @@ module TeamStats
       hash
     end
   end
-  
+
   def best_season(team_id)
     final_percentage = team_season_wins_hash(team_id)
-    answer = final_percentage.max_by{|season,win_percentage| win_percentage}
+    answer = final_percentage.max_by{|season, win_percentage| win_percentage}
     answer[0]
   end
 
   def worst_season(team_id)
     final_percentage = team_season_wins_hash(team_id)
-    answer = final_percentage.min_by{|season,win_percentage| win_percentage}
+    answer = final_percentage.min_by{|season, win_percentage| win_percentage}
     answer[0]
   end
 
   def team_season_wins_hash(team_id)
     percentage_seasons = Hash.new(0)
     @seasons.teams[team_id].each do |season|
+
       season_key = season[0]
-      team_season = season[1].flatten!
+      team_season = season[1].flatten
       stats = win_lose_draw_regular_season(team_id, team_season)
-      final_percentage = team_season_win_percentage(stats, team_id)
+      final_percentage = best_worst_win_percentage(stats, team_id)
       percentage_seasons[season_key] = final_percentage[:win_percentage]
     end
 
     percentage_seasons
   end
 
-  def team_win_percentage_season(hash, team_id)
+  def best_worst_win_percentage(hash, team_id)
     total_games = (hash[:win] + hash[:loss] + hash[:draw])
     wins = hash[:win]
     percentage = ((wins.to_f / total_games) * 100).round(2)
